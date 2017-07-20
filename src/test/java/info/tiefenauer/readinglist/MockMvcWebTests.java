ceea4e0266473264dd10ea5302cf67394a8709d3
Daniel Tiefenauer <git@tiefenauer.info>
2017-07-20 15:35:02 +0200

    add web test

---
+++ src/test/java/info/tiefenauer/readinglist/MockMvcWebTests.java
@@ -0,0 +1,45 @@
+package info.tiefenauer.readinglist;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class MockMvcWebTests {
+
+    @Autowired
+    private WebApplicationContext webContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setupMockMvc(){
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webContext)
+                .build();
+    }
+
+    @Test
+    public void homePage() throws Exception {
+        mockMvc.perform(get("/readingList/dani"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+                .andExpect(model().attributeExists("books"))
+                .andExpect(model().attribute("books", is(empty())));
+    }
+
+}
