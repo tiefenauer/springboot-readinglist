1dc4a76dd05ac70942cd562319ade771ed89a4b6
Daniel Tiefenauer <git@tiefenauer.info>
2017-07-20 17:30:14 +0200

    add web tests for security

--- src/test/java/info/tiefenauer/readinglist/MockMvcWebTests.java
+++ src/test/java/info/tiefenauer/readinglist/MockMvcWebTests.java
@@ -1,11 +1,16 @@
 package info.tiefenauer.readinglist;
 
 import info.tiefenauer.readinglist.entity.Book;
+import info.tiefenauer.readinglist.entity.Reader;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,13 +19,13 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ReadinglistApplication.class)
 @WebAppConfiguration
 public class MockMvcWebTests {
 
@@ -33,8 +38,46 @@ public class MockMvcWebTests {
     public void setupMockMvc() {
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(webContext)
+                .apply(springSecurity())
                 .build();
     }
+
+    @Test
+    public void homePage_unauthenticatedUser() throws Exception{
+        mockMvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().string("Location", "http://localhost/login"));
+    }
+
+    @Ignore("assertion fails and I have no idea why...")
+    @Test
+    @WithMockUser(username="dani", password="password", roles="READER")
+    public void homePage_authenticatedUser2() throws Exception {
+        // arrange
+        Reader expectedReader = createReader("dani", "password", "Daniel Tiefenauer");
+        //act
+        mockMvc.perform(get("/readingList/dani"))
+        //assert
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList"))
+                .andExpect(model().attribute("reader",samePropertyValuesAs(expectedReader)))
+                .andExpect(model().attribute("books", hasSize(0)));
+    }
+
+    @Ignore("Context initialisation fails and I have no idea why...")
+    @Test
+    @WithUserDetails("dani")
+    public void homePage_authenticateUser() throws Exception{
+        // arrange
+        Reader expectedReader = createReader("dani", "password", "Daniel Tiefenauer");
+        //
+        mockMvc.perform(get("/readingList/dani"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("readingList/craig"))
+                .andExpect(model().attribute("reader", samePropertyValuesAs(expectedReader)))
+                .andExpect(model().attribute("books", hasSize(0)));
+    }
+
 
     @Test
     public void homePage() throws Exception {
@@ -90,6 +133,14 @@ public class MockMvcWebTests {
         book.setIsbn(isbn);
         book.setDescription(description);
         return book;
+    }
+
+    private Reader createReader(String username, String password, String fullName) {
+        Reader reader = new Reader();
+        reader.setUsername(username);
+        reader.setPassword(password);
+        reader.setFullname(fullName);
+        return reader;
     }
 
 }
